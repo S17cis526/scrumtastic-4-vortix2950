2d0209a0b8b7a60f6136812f1f40cebde2fbbfa6
Nathan Bean <nhbean@ksu.edu>
2017-03-09 14:22:36 -0600

    in-class work, 2-9

--- app.js
+++ app.js
@@ -1,18 +1,44 @@
 "use strict";
 
+var PORT = 3000;
 
+var fs = require('fs');
+var http = require('http');
 var sqlite3 = require('sqlite3').verbose();
 var db = new sqlite3.Database('scrumtastic.sqlite3', function(err) {
   if(err) console.error(err);
 });
 
+var router = new (require('./lib/route').Router);
+
+router.get('/', function(req, res) {
+  fs.readFile('public/index.html', function(err, body){
+    res.end(body);
+  });
+});
+
+router.get('/app.js', function(req, res) {
+  fs.readFile('public/app.js', function(err, body){
+    res.end(body);
+  });
+});
+
+router.get('/projects', function(req, res) {
+  db.all('SELECT * FROM projects', [], function(err, projects){
+    res.setHeader('Content-Type', 'text/json');
+    res.end(JSON.stringify(projects));
+  });
+});
+
 var migrate = require('./lib/migrate');
 migrate(db, 'migrations', function(err){
-  db.serialize(function(){
-    db.run("INSERT INTO projects (name) values (?)", ['title']);
-    db.all("SELECT * FROM projects", [], function(err, rows){
-      if(err) return console.error(err);
-      console.log("ROWS:", rows);
-    });
+
+  var server = new http.Server(function(req, res) {
+    router.route(req, res);
   });
+  server.listen(PORT, function(){
+    console.log("listening on port " + PORT);
+  });
+
+
 });
